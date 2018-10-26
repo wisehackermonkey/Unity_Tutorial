5d6ccfd17660ac9aecc73e9be5c7cfbeed26f97b
Oran C <oranbusiness@gmail.com>
2018-10-26 01:38:38 -0700

    Add files via upload

---
+++ Assets/move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+	//movement speed of character
+	public float speed = 10f;
+
+	void Start () {
+	}
+
+	void Update () {
+		//MOVE character left and right 
+		//"d" key do action
+				if(Input.GetKey("d")){
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if(Input.GetKey("a")){
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+	}
+}
